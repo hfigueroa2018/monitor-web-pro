6e537dd85200d2b1bd9deb17153830bc8cf5240a
hfigu <hectorfigueroacarrasco1@gmail.com>
2025-09-15 15:49:07 -0300

    Agrega telegram_notifier.py faltante

---
+++ telegram_notifier.py
@@ -0,0 +1,12 @@
+import requests
+from config import TELEGRAM_TOKEN, TELEGRAM_CHAT_ID
+
+def send_telegram(message: str):
+    url = f"https://api.telegram.org/bot{8305256746:AAGeJmNUDLyO-8MTEEMpVoFFO863U9I2zW8}/sendMessage"
+    payload = {"chat_id": 5646679766, "text": message}
+    try:
+        r = requests.post(url, json=payload, timeout=10)
+        return r.ok
+    except Exception as e:
+        print("Error enviando Telegram:", e)
+        return False
